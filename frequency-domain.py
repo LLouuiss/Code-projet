7ab14e5de607dfc9da086ab3a92592700e5bd276
michauxba <bastien.michaux@student.uclouvain.be>
2024-11-12 23:52:05 +0100

    end

--- frequency-domain.py
+++ frequency-domain.py
@@ -55,21 +55,30 @@ Input:
     x: np.array frequency
     y: np.array acceleration in frequency domain
     output_name: string name of the output file
+    boundary: float remove the extremities of the graph where abs(y) is smaller than boundary
 Output:
     None
 """
-def plot_frequency_domain(x, y, output_name):
+def plot_frequency_domain(x, y, output_name, boundary = 2e-2):
     plt.figure(output_name + "_f", figsize=(30,21))
+    #Real part
     plt.subplot(211)
     plt.plot(x, np.real(y), 'b', label='Real')
-    plt.xlim(x[np.abs(y)>1e-3][0], x[np.abs(y)>1e-3][-1])
+    try:
+        plt.xlim(x[np.abs(y)>boundary][0], x[np.abs(y)>boundary][-1])
+    except IndexError:
+        plt.xlim(x[3*x.size//8], x[5*x.size//8])
     plt.grid(True)
     plt.legend()
     plt.xlabel('Frequency [Hertz]')
     plt.ylabel('Acceleration [m/s²]')
+    #Imaginary part
     plt.subplot(212)
     plt.plot(x, np.imag(y), 'r', label='Imaginary')
-    plt.xlim(x[np.abs(y)>1e-3][0], x[np.abs(y)>1e-3][-1])
+    try:
+        plt.xlim(x[np.abs(y)>boundary][0], x[np.abs(y)>boundary][-1])
+    except IndexError:
+        plt.xlim(x[3*x.size//8], x[5*x.size//8])
     plt.grid(True)
     plt.legend()
     plt.xlabel('Frequency [Hertz]')
@@ -113,6 +122,8 @@ if __name__ == '__main__':
     m = 0.812 #kg
     #Natural Cyclic Frequency
     omega = (K/m)**.5
+    f = omega/(2*math.pi)
+    print(f"Natural frequency: {f:.2f} Hertz")
     
     #Time
     n = 2**16-100
@@ -141,11 +152,12 @@ if __name__ == '__main__':
         x, y = frequency_domain(t, a, True)
         plot_frequency_domain(x, y, output_name[0])
         plot_time_domain(t, a, output_name[0])
-
+    boundary = [2e-2, 1e-2, 1e-2, 1e-2]
     #Point k
     if False:    
         #Values of xi
-        for xi in [1.39e-3, 2e-2, 5e-2, 1e-1]:
+        for i, xi in enumerate([1.39e-3, 2e-2, 5e-2, 1e-1]):
+            print(f"xi: {xi:.2e} => fd = {f*(1-xi**2)**.5:.2f} Hertz")
             #Time
             t = np.arange(0, n*h, h)
             #damped natural frequency
@@ -154,7 +166,7 @@ if __name__ == '__main__':
             ddu = xi**2 * omega**2 * np.exp(-xi*omega*t) * (u_0 * np.cos(omega_d*t) + xi*omega*u_0 * np.sin(omega_d*t)/omega_d) - 2 * xi * omega * np.exp(-xi*omega*t) * (-u_0 * omega_d * np.sin(omega_d*t) + xi*omega*u_0 * np.cos(omega_d*t)) + np.exp(-xi*omega*t) * (-u_0 * omega_d**2 * np.cos(omega_d*t) - xi*omega*u_0 * omega_d * np.sin(omega_d*t))
             
             x, y = frequency_domain(t, ddu, True)
-            plot_frequency_domain(x, y, "4-2"+"-xi="+str(xi))
+            plot_frequency_domain(x, y, "4-2"+"-xi="+str(xi), boundary[i])
             plot_time_domain(t,ddu, "4-2"+"-xi="+str(xi))
 
     #Point l
@@ -176,7 +188,7 @@ if __name__ == '__main__':
             t = t[i_start[i]:i_end[i]]
             a = a[i_start[i]:i_end[i]]
             x,y = frequency_domain(t,a, True)
-            plot_frequency_domain(x,y,"4-3-omega_="+str(omega__[i])+"_exp")
+            plot_frequency_domain(x,y,"4-3-omega_="+str(omega__[i])+"_exp", 5e-2)
             plot_time_domain(t,a,"4-3-omega_="+str(omega__[i])+"_exp")
 
             #Analytical part
@@ -184,16 +196,16 @@ if __name__ == '__main__':
             phi = math.atan(2*xi*omega__[i]/(1-omega__[i]**2))
             R_d = 1/math.sqrt((1-omega__[i]**2)**2 + (2*xi*omega__[i])**2)
             R_a = omega__[i]**2*R_d
-            ddu = p_0/m*R_a*np.sin(omega__[i]*omega*t-phi)
+            ddu = - p_0/m*R_a*np.sin(omega__[i]*omega*t-phi)
             x,y = frequency_domain(t, ddu, True)
-            plot_frequency_domain(x,y,"4-3-omega_="+str(omega__[i])+"_ana")
+            plot_frequency_domain(x,y,"4-3-omega_="+str(omega__[i])+"_ana", 5e-3)
             plot_time_domain(t,ddu,"4-3-omega_="+str(omega__[i])+"_ana")
 
 
     #Point m
     #Window of the data
     di = 25*f_acc
-    if False:
+    if True:
         for i in range(len(omega__)):
             t, a = read_file(file_name[i + 1], f_acc)
             t = t[i_start[i]:i_start[i] + di]
@@ -201,4 +213,4 @@ if __name__ == '__main__':
             x,y = frequency_domain(t,a, True)
             plot_frequency_domain(x,y,"4-4-omega_="+str(omega__[i])+"_exp")
             plot_time_domain(t,a,"4-4-omega_="+str(omega__[i])+"_exp")
-    plt.show()
+    #plt.show()
