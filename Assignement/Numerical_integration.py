846a6ae7175b1e9642e1c36f941165c63234af12
michauxba <bastien.michaux@student.uclouvain.be>
2024-11-28 12:11:03 +0100

    video

--- Assignement/Numerical_integration.py
+++ Assignement/Numerical_integration.py
@@ -2,27 +2,80 @@ from functions import *
 
 # case 1
 #Initialize matrix
-m = 0.2 * np.ones(6)
-l = 0.75/6 * np.ones(6)
+nb = 6
+m = 0.2 * np.ones(nb)
+l = 0.75/nb * np.ones(nb)
 K = stiffness_matrix(m, l)
 M = mass_matrix(m, l)
-C = np.zeros((6,6))
-u_0 = np.zeros((6,2))
+C = np.zeros((nb,nb))
+u_0 = np.zeros((nb,2))
 
-t = np.linspace(0, 20, 1000)
-P = np.zeros((6, t.shape[0]))
+t = np.linspace(0, 20, 24*20)
+P = np.zeros((nb, t.shape[0]))
 u_0[0][0] = 30*math.pi/180
 #Resolve and plot
 if False:
     U = Newmark_Method_M(t, u_0, P, K, M, C)
     plot_M(t, U[:,0], "Undamped-free-vibration-Case-1", "Time [s]", "Angle [rad]")
 
+def init():
+    ax = plt.gca()
+    ax.xaxis.tick_top()
+    ax.xaxis.set_label_position('top')
+    plt.grid()
+    plt.xlabel("x [m]")
+    plt.ylabel("y [m]")
+    plt.xlim((-.75,.4))
+    plt.ylim((-.8,0))
+
+line = []
+def update(frame, U):
+    x = np.zeros(U.shape[0]+1)
+    y = np.zeros(U.shape[0]+1)
+    for i in range(1,x.size):
+        x[i] = x[i - 1] + .75/U.shape[0]*math.sin(U[i-1][frame])
+        y[i] = y[i - 1] - .75/U.shape[0]*math.cos(U[i-1][frame])
+    if len(line)>0:
+        l = line.pop(0)
+        l.remove()
+    line.append(plt.plot(x,y,"-b")[0])
+
+if False:
+    fig = plt.figure()
+    generate_vid(fig, update, init, t, U[:,0], "Undamped-free-vibration-Case-1.mp4")
+
+#Damping
 c = 200 * 1e-4 * np.ones(6)
 C = damping_matrix(c)
-print(M)
-print(K)
-print(C)
+Phi__ = np.column_stack((np.hstack(K), np.hstack(M)))
+alpha = solve(Phi__.T @ Phi__,Phi__.T @ np.hstack(C))
+C = alpha[0]*K + alpha[1]*M
 
-if True:
+if False:
     U = Newmark_Method_M(t, u_0, P, K, M, C)
     plot_M(t, U[:,0], "Damped-free-vibration-Case-1", "Time [s]", "Angle [rad]")
+
+if False:
+    fig = plt.figure()
+    generate_vid(fig, update, init, t, U[:,0], "Damped-free-vibration-Case-1.mp4")
+
+#Case 2
+nb = 3
+m = 0.4 * np.ones(nb)
+l = 0.75/nb * np.ones(nb)
+K = stiffness_matrix(m, l)
+M = mass_matrix(m, l)
+C = np.zeros((nb,nb))
+u_0 = np.zeros((nb,2))
+
+t = np.linspace(0, 20, 1000)
+P = np.zeros((nb, t.shape[0]))
+u_0[0][0] = 30*math.pi/180
+#Resolve and plot
+if False:
+    U = Newmark_Method_M(t, u_0, P, K, M, C)
+    plot_M(t, U[:,0], "Undamped-free-vibration-Case-2", "Time [s]", "Angle [rad]")
+
+if False:
+    fig = plt.figure()
+    generate_vid(fig, update, init, t, U[:,0], "Undamped-free-vibration-Case-2.mp4")
--- Assignement/functions.py
+++ Assignement/functions.py
@@ -1,7 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.linalg import eig
+from scipy.linalg import eig,solve
 import math
+from matplotlib.animation import FuncAnimation
+from functools import partial
 
 fontsize = 10
 plt.rc('font', size = fontsize)
@@ -27,11 +29,11 @@ def Newmark_Method_S(t, u_0, p, m, k, c):
     U[0][0] = u_0[0]
     U[1][0] = u_0[1]
     dt = t[1] - t[0]
+    k_b = m/beta/dt**2 + c*gamma/beta/dt + k
     U[2][0] = (p[0] - c*U[1][0] - k*U[0][0])/m
     for i in range(0, t.shape[0]-1):
         du_p = U[1][i] + dt*(1 - gamma)*U[2][i]
         u_p = U[0][i] + dt*U[1][i] + dt**2*(1/2 - beta)*U[2][i]
-        k_b = m/beta/dt**2 + c*gamma/beta/dt + k
         p_b = p[i + 1] + m * u_p/beta/dt**2 + c*(gamma * u_p/beta/dt - du_p)
         U[0][i + 1] = p_b/k_b
         U[2][i + 1] = (U[0][i + 1] - u_p) / (beta*dt**2)
@@ -55,19 +57,18 @@ def Newmark_Method_M(t, u_0, P, K, M, C):
     n = len(t)
     U = np.zeros((N, 3, n))
     Q = np.zeros((N, 3, n))
-    phi = phi_normalized_M(K, M)
+    w, phi = phi_normalized_M(K, M)
     phi_inv = np.linalg.inv(phi)
-    K_g = phi.T @ K @ phi
     C_g = phi.T @ C @ phi
-    print(C_g)
     P_g = phi_inv @ P
     q_0 = np.zeros((N,2))
     for j in range(2):
         q_0[:,j] = phi_inv @ u_0[:,j] 
-    if isdiag(C_g, 1e-15)==False:
+    if isdiag(C_g, 1e-12)==False:
         print("C_g is not diagonal")
+        print(C_g)
     for i in range(N):
-        Q[i] = Newmark_Method_S(t, q_0[i], P_g[i], 1, K_g[i][i], C_g[i][i])
+        Q[i] = Newmark_Method_S(t, q_0[i], P_g[i], 1, w[i]**2, C_g[i][i])
     for j in range(3):
         U[:,j] = phi @ Q[:,j]
     return U
@@ -81,7 +82,7 @@ def normalize_mass(phi, M):
 def phi_normalized_M(K, M):
     eig_val, eig_vec = eig(K, M)
     phi = normalize_mass(eig_vec, M)
-    return phi
+    return np.real(eig_val)**.5, phi
 
 def isdiag(A, tol):
     for i in range(A.shape[0]):
@@ -133,6 +134,13 @@ def plot_M(t, U, title, xlabel, ylabel ):
         plt.plot(t, U[i], label="Node "+str(i))
     plt.xlim((0,20))
     plot_end(f"Assignement/figure/{title}.pdf")
+
+
+def generate_vid(fig, func, init, t, D, filename):
+    print('start_animation')
+    ani = FuncAnimation(fig, partial(func, U=D), frames=np.arange(0,t.size), init_func=init, interval=(t[1]-t[0])*1e-3)
+    ani.save("Assignement/vid/" + filename, fps=1/(t[1]-t[0]), dpi=300)
+    print("End animation")
 
 if __name__=="__main__":
     print("Wrong Code!!!")
